4278cdd36d33425f0d057c354c8ee62558b72477
KaTaai <powerlinch@hotmail.com>
2014-07-01 01:48:20 +0200

    basic user input

--- src/CalculatePlanetyStuff/massCalculator.java
+++ src/CalculatePlanetyStuff/massCalculator.java
@@ -1,10 +1,14 @@
 package CalculatePlanetyStuff;
 
+import java.util.Scanner;
+
 public class massCalculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Planet aarde=new Planet("Aarde", 3, 3);
+		Scanner keyboard=new Scanner(System.in);
+		System.out.println("Give the name of the planet followed by its radius followed by the average density of the planet separated by spaces");
+		Planet aarde=new Planet(keyboard.next(), keyboard.nextLong(), keyboard.nextLong());
 		System.out.println(aarde.getName());
 	}
 
