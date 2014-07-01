432456a8e2886c10365f0b56e1ffc90b8f4fd202
KaTaai <powerlinch@hotmail.com>
2014-07-01 02:27:40 +0200

    User input planets complete

--- src/CalculatePlanetyStuff/massCalculator.java
+++ src/CalculatePlanetyStuff/massCalculator.java
@@ -7,9 +7,17 @@ public class massCalculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyboard=new Scanner(System.in);
-		System.out.println("Give the name of the planet followed by its radius followed by the average density of the planet separated by spaces");
-		Planet aarde=new Planet(keyboard.next(), keyboard.nextLong(), keyboard.nextLong());
-		System.out.println(aarde.getName());
+		System.out.println("How many kg does the object weigh?");
+		int weightObject=keyboard.nextInt();
+		System.out.println("How many planets are there?");
+		int totalPlanets=keyboard.nextInt();
+		Planet planets[]=new Planet[totalPlanets];
+		System.out.println("Give the name of the planet"+(planets.length>1?"s ":" ")+"followed by its radius followed by the average density of the planet separated by spaces");
+		for(int i=0;i<planets.length;i++){
+			planets[i]=new Planet(keyboard.next(), keyboard.nextLong(), keyboard.nextLong());
+		}
+		
+		
 	}
 
 }
